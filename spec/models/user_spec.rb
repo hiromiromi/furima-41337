fdb8ee13d20e685def531affe4e274e4c8b70182
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2024-11-14 00:55:55 +0900

    Gimei導入、exampleの記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'nickname,email,password,password_confirmation,
+        last_name,first_name,last_name_kana,first_name_kana,birthdayが
+        存在すれば登録できる' do
+    end
+
+    it 'nicknameが空では登録できない' do
+    end
+
+    it 'emailが空では登録できない' do
+    end
+
+    it '重複したemailが存在する場合は登録できない' do
+    end
+
+    it 'emailは@を含まないと登録できない' do
+    end
+
+    it 'passwordが空では登録できない' do
+    end
+
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+    end
+
+    it 'passwordが5文字以下では登録できない' do
+    end
+
+    it 'passwordが129文字以上では登録できない' do
+    end
+
+    it 'passwordは半角英数字混合でないと登録できない' do
+    end
+
+    it 'last_nameが空では登録できない' do
+    end
+
+    it 'last_nameは全角でないと登録できない' do
+    end
+
+    it 'first_nameが空では登録できない' do
+    end
+
+    it 'first_name_kanaは全角でないと登録できない' do
+    end
+
+    it 'last_name_kanaが空では登録できない' do
+    end
+
+    it 'last_name_kanaは全角（カタカナ）でないと登録できない' do
+    end
+
+    it 'first_name_kanaが空では登録できない' do
+    end
+
+    it 'first_name_kanaは全角（カタカナ）でないと登録できない' do
+    end
+
+    it 'birthdayが空では登録できない' do
+    end
+
+  end
 end
