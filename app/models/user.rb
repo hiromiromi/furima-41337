656538552f3a01ae03d70537a33e7b520826dfca
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2024-11-13 17:33:39 +0900

    バリデーション（本人確認）編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,13 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角文字(カタカナ）を使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday, presence: true
 end
