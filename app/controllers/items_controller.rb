2f80c3dc5ce6c8e86f36792f0844d758828b5345
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2024-11-19 11:33:54 +0900

    バリデーション、アソシエーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,13 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:items).permit(:image, :item, :item_description, :category_id, :item_condition_id, :shipping_cost_id, :region_id, :number_of_day_id, :price)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,20 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_cost
+  belongs_to :region
+  belongs_to :number_of_day
+
+  validates :image, :item, :item_description, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "Category can't be blank"}
+  validates :item_condition_id, numericality: { other_than: 1 , message: "Item condition can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "Shipping cost can't be blank"}
+  validates :region_id, numericality: { other_than: 1 , message: "Region can't be blank"}
+  validates :number_of_day_id, numericality: { other_than: 1 , message: "Number of days can't be blank"}
+  
+  validates :price , numericality: {greater_than: 50,less_than: 250}
 end
