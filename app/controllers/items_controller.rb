62a78cba4cdf9cb01bad849aaa16fe5b40c6f6f1
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2024-11-19 12:42:46 +0900

    viewのhoge直し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, satatus: :unprocessable_entity
+    end
   end
 
   private
+
   def item_params
-    params.require(:items).permit(:image, :item, :item_description, :category_id, :item_condition_id, :shipping_cost_id, :region_id, :number_of_day_id, :price)
+    params.require(:items).permit(:image, :item, :item_description, :category_id, :item_condition_id, :shipping_cost_id, :region_id, :number_of_day_id, :price).merge(user_id: current_user.id)
   end
 end
