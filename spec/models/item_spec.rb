db42109fd21b5a93b661e2cad4e4a834336c160b
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2024-11-20 11:51:00 +0900

    Itemモデル単体テスト修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -66,7 +66,6 @@ RSpec.describe Item, type: :model do
       it 'priceが空では登録できない' do
         @item.price = ''
         @item.valid?
-        binding.pry
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
