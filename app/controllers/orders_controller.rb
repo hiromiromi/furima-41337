1afc744b339c848c4d0c7c3cf6d7ac6e5e26523b
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2024-11-29 17:38:31 +0900

    view,controller記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index]
   def index
     @order_address = OrderAddress.new
+  end
+
+  def create
+    
+  end
+
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
