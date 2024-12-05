daf1035cd19411c49f0b3d683752e4b4f5f2ed63
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2024-12-05 15:33:10 +0900

    render失敗後修正３

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_29_062819) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_05_063125) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
